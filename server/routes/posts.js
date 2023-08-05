1aa038e30cd0335a2a75d7ca911370eb64a868fd
Aditya <mandaladitya901@gmail.com>
2023-08-05 20:19:23 +0530

    first commit

---
+++ server/routes/posts.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { addCommentToPost, getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/comment", verifyToken, addCommentToPost);
+
+export default router;
