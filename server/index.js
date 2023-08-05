b5b1db7eb86d355fb51a269653284f8777df64cf
Aditya <mandaladitya901@gmail.com>
2023-08-05 22:59:00 +0530

    fixed server error

--- server/index.js
+++ server/index.js
@@ -29,7 +29,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ["https://connnectverse.vercel.app/"],
+  credentials: true
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
