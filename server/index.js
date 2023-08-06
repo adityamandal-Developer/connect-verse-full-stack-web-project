e179c90276138d7c471e3bd94f6503f1e40bdb77
Aditya <mandaladitya901@gmail.com>
2023-08-06 16:08:24 +0530

    fixed cors issue

--- server/index.js
+++ server/index.js
@@ -30,8 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: ["https://connnectverse.vercel.app/"],
-  credentials: true
+  origin: ["https://connnectverse.vercel.app/", "http://localhost:3000/"],
 }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
